d69a1c0428c18b70c3b2d43c952e2e0a3a70124d
yasdev08 <yasser.mechrem29@gmail.com>
2024-12-02 05:45:33 +0100

    sitemap && robots fix

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '',
     },
-    sitemap: 'https://cpservice.com/sitemap.xml',
+    sitemap: 'https://cpservice.pro/sitemap.xml',
   }
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://cpservice.com',
+      url: 'https://cpservice.pro',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://cpservice.com/about',
+      url: 'https://cpservice.pro/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://cpservice.com/services',
+      url: 'https://cpservice.pro/services',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
