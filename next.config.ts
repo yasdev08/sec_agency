adc83de88dbb64cfbd0081ad4bfd3ff8457f6170
yasdev08 <yasser.mechrem29@gmail.com>
2024-12-03 01:08:24 +0100

    redirect error

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: '/(.*)',
-        destination: 'https://www.cpservice.pro/',
-        permanent: true, // Change this to true for a 308/301 redirect
-      },
-    ];
-  }
 };
 
 export default nextConfig;
